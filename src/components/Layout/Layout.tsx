1ccf94bc1e5e06db606486c7d7119057cc2a6a14
Fuad <fuadbayramv@gmail.com>
2023-10-18 20:45:24 +0400

    components coverted class to func

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,17 @@
-import { Component } from "react";
 import { Outlet } from "react-router-dom";
 import SideBar from "../SideBar/SideBar";
 import "./Layout.scss";
 
-export class Layout extends Component {
-  render() {
-    return (
-      <div className="layout__container">
-        <SideBar />
+const Layout = () => {
+  return (
+    <div className="layout__container">
+      <SideBar />
 
-        <div className="outlet__container">
-          <Outlet />
-        </div>
+      <div className="outlet__container">
+        <Outlet />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Layout;
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,12 +1,5 @@
-import { Component } from "react";
 import "./SideBar.scss";
 import { Link } from "react-router-dom";
-
-type SideBarProps = object;
-
-type SideBarState = {
-  routes: Routes[];
-};
 
 type Routes = {
   name: string;
@@ -14,46 +7,41 @@ type Routes = {
   icon: string;
 };
 
-export class SideBar extends Component<SideBarProps, SideBarState> {
-  state: SideBarState = {
-    routes: [
-      {
-        name: "Dashboard",
-        path: "/dashboard",
-        icon: "",
-      },
-      {
-        name: "Link1",
-        path: "/dashboard",
-        icon: "",
-      },
-      {
-        name: "Link2",
-        path: "/dashboard",
-        icon: "",
-      },
-      {
-        name: "Link3",
-        path: "/dashboard",
-        icon: "",
-      },
-    ],
-  };
+const routes: Routes[] = [
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+    icon: "",
+  },
+  {
+    name: "Link1",
+    path: "/dashboard",
+    icon: "",
+  },
+  {
+    name: "Link2",
+    path: "/dashboard",
+    icon: "",
+  },
+  {
+    name: "Link3",
+    path: "/dashboard",
+    icon: "",
+  },
+];
 
-  render() {
-    const { routes } = this.state;
-    return (
-      <div className="sidebar__container">
-        <ul className="link__list">
-          {routes.map(({ name, path }) => (
-            <li key={name} className="link">
-              <Link to={path}>{name}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const SideBar = () => {
+  return (
+    <div className="sidebar__container">
+      <ul className="link__list">
+        {routes.map(({ name, path }) => (
+          <li key={name} className="link">
+            <Link to={path}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default SideBar;
