83934c545aa1b5ae60f16e2d55f4a21d3228de0d
Fuad <fuadbayramv@gmail.com>
2023-10-29 23:26:33 +0400

    car enums added

--- src/types/index.ts
+++ src/types/index.ts
@@ -33,3 +33,55 @@ export type DataWithPagination<D> = {
   data: D;
   pagination: Pagination;
 };
+
+export enum DriveUnit {
+  Full = 1,
+  Fron = 2,
+  Back = 3,
+}
+
+export enum TransmissionType {
+  Empty = 0,
+  Mechanical = 1,
+  Automatic = 2,
+  Robot = 3,
+  Variable = 4,
+}
+
+export enum EngineType {
+  Petrol = 1,
+  Diesel = 2,
+  Gas = 3,
+  Electro = 4,
+  Hybrid = 5,
+  PlugInHybrid = 6,
+}
+
+export enum BodyType {
+  Sedan = 1,
+  Hatchback = 2,
+  Liftback = 3,
+  SUV = 4,
+  StationWagon = 5,
+  Coupe = 6,
+  Minivan = 7,
+  Pickup = 8,
+  Limousine = 9,
+  Van = 10,
+  Cabriolet = 11,
+  Bus = 12,
+  Minibus = 13,
+  Track = 14,
+  Motorcycle = 15,
+  Agricultural = 16,
+  Trailer = 17,
+  Crane = 18,
+  Loader = 19,
+  GolfCar = 20,
+  Universal = 21,
+  Crossover = 22,
+  Quadrocycle = 23,
+  Scooter = 24,
+  ConstructionEquipment = 25,
+  Caravan = 26,
+}
