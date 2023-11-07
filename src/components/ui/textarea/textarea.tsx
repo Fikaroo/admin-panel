6479b159457e734ba4d63ec906d9dc3413855a74
DJavidM <cavid_17_94@mail.ru>
2023-11-08 00:43:41 +0400

    info

---
+++ src/components/ui/textarea/textarea.tsx
@@ -0,0 +1,30 @@
+import "./textarea.scss"
+const TextArea = ({
+  title,
+  subtitle,
+  maxSymbol,
+  value,
+  onChange,
+}: {
+  title: string;
+  subtitle: string;
+  maxSymbol: string;
+  value: string;
+  onChange: (e: string) => void;
+}) => {
+  return (
+    <div style={{ marginBlock: 32 }}>
+      <label className="textAreaTitle">{title}</label>
+      <p className="textAreaSubTitle">{subtitle}</p>
+      <textarea
+        className="textareaElement"
+        value={value}
+        maxLength={500}
+        style={{ marginTop: 6 }}
+        onChange={(event) => onChange(event.target.value)}
+      />
+      <p className="maxSymbol">{maxSymbol}</p>
+    </div>
+  );
+};
+export default TextArea;
--- src/pages/Info/InfoAbout/InfoAbout.tsx
+++ src/pages/Info/InfoAbout/InfoAbout.tsx
@@ -1,6 +1,87 @@
+import { useState, useContext } from "react";
+import { LocalizationContext } from "@/hooks/customLangHook";
+import LaguageSwitcher from "@/elements/laguageSwitcher";
+import TextArea from "@/components/ui/textarea/textarea";
+import FilledButton from "@/elements/filledButton";
+
 const InfoAbout = () => {
-    return(
-        <p>Sal</p>
-    )
-}
+  const [aboutValueRu, setAboutValueRu] = useState("");
+  const [b2bValueRu, setB2bValueRu] = useState("");
+  const [aboutValueAz, setAboutValueAz] = useState("");
+  const [b2bValueAz, setB2bValueAz] = useState("");
+  const [aboutValueEn, setAboutValueEn] = useState("");
+  const [b2bValueEn, setB2bValueEn] = useState("");
+
+  const saveRuData = () => {};
+  const saveAzData = () => {};
+  const saveEnData = () => {};
+
+  const { currentLanguage, setCurrentLanguage, translate } =
+    useContext(LocalizationContext);
+
+  return (
+    <div>
+      <LaguageSwitcher>
+        {currentLanguage === "ru" ? (
+          <div>
+            <TextArea
+              title={"О Нас"}
+              subtitle={"текст"}
+              maxSymbol={"Максимум 500 символов"}
+              value={aboutValueRu}
+              onChange={(ev: string) => setAboutValueRu(ev)}
+            />
+            <TextArea
+              title={"B2B"}
+              subtitle={"текст"}
+              maxSymbol={"Максимум 500 символов"}
+              value={b2bValueRu}
+              onChange={(ev: string) => setB2bValueRu(ev)}
+            />
+            <FilledButton
+              text={"Сохранить изменения"}
+              onClick={() => saveRuData()}
+            />
+          </div>
+        ) : currentLanguage === "az" ? (
+          <div>
+            <TextArea
+              title={"Haqqimizda"}
+              subtitle={"mətn"}
+              maxSymbol={"Maksimum 500 simvol"}
+              value={aboutValueAz}
+              onChange={(ev: string) => setAboutValueAz(ev)}
+            />
+            <TextArea
+              title={"B2B"}
+              subtitle={"mətn"}
+              maxSymbol={"Maksimum 500 simvol"}
+              value={b2bValueAz}
+              onChange={(ev: string) => setB2bValueAz(ev)}
+            />
+            <FilledButton text={"Yadda saxla"} onClick={() => saveAzData()} />
+          </div>
+        ) : (
+          <div>
+            <TextArea
+              title={"About Us"}
+              subtitle={"text"}
+              maxSymbol={"Maximum 500 symbol"}
+              value={aboutValueEn}
+              onChange={(ev: string) => setAboutValueEn(ev)}
+            />
+            <TextArea
+              title={"B2B"}
+              subtitle={"text"}
+              maxSymbol={"Maximum 500 symbol"}
+              value={b2bValueEn}
+              onChange={(ev: string) => setB2bValueEn(ev)}
+            />
+            <FilledButton text={"Save changes"} onClick={() => saveEnData()} />
+          </div>
+        )}
+      </LaguageSwitcher>
+    </div>
+  );
+};
 export default InfoAbout;
